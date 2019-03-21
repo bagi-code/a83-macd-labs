2e47166ef8f69281f10c8ebb86f1624d8babade8
Bagicode <rdp10007@gmail.com>
2019-03-21 22:04:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     $host = "submissionrobbypertama.database.windows.net";
     $user = "robby";
     $pass = "Bagicode@29";
-    $db = "dbo.Registration";
+    $db = "Registration";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
